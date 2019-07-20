17302be53a479d1f8e07fde1f21b106603905725
Krystian Jacaszek <krystian.jacaszek@gmail.com>
2019-07-20 11:37:26 +0200

    -Fix Router
    -Fix routing between components

--- AngularWebApp/src/app/range.spec.ts
+++ AngularWebApp/src/app/range.spec.ts
@@ -1,7 +1,8 @@
 import { Range } from './range';
-
+let x=0;
+let y=10;
 describe('Range', () => {
   it('should create an instance', () => {
-    expect(new Range()).toBeTruthy();
+    expect(new Range(x,y)).toBeTruthy();
   });
 });
